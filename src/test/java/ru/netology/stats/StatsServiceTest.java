fbef0bf748f01333a3121d84bfb0b3117be629f2
Abhay <kiyashkoandrey1988@gmail.com>
2023-06-23 11:58:42 +0300

    Сумма всех продаж - days -sales

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -3,16 +3,16 @@ package ru.netology.stats;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-
 public class StatsServiceTest {
-    @Test
-    public void sumDay() {
-        StatsService service = new StatsService();
-        long[] days = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
-        long expectedSumDays = 180;
-        long actualSumDays = service.getSumSales(days);
 
-        Assertions.assertEquals(expectedSumDays, actualSumDays);
+    @Test
+    public void sumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+        long actual = service.getSumSales(sales);
+
+        Assertions.assertEquals(expected, actual);
     }
 }
 
