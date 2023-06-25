d25fab151aa79f36f4071e2288c4c4a6e69cd915
Abhay <kiyashkoandrey1988@gmail.com>
2023-06-25 11:57:59 +0300

    Min month fnd max month sales

--- src/main/java/ru/netology/stats/StatsService.java
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -17,14 +17,36 @@ public class StatsService {
         for (int i = 0; i < sales.length; i++) {
             sumSales += sales[i];
         }
-            long middleSales = sumSales/sales.length;
+        long middleSales = sumSales / sales.length;
 
 
         return middleSales;
     }
 
+    public int maxMonSales(long[] sales) {
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+
+        return maxMonth + 1;
     }
 
+    public int minMonSales(long[] sales) {
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+
+        return minMonth + 1;
+    }
+}
 
 
 
--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -14,6 +14,7 @@ public class StatsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
     public void getMiddleSales() {
         StatsService service = new StatsService();
@@ -23,6 +24,27 @@ public class StatsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void maxMonSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 8;
+        long actual = service.maxMonSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    public void minMonSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 9;
+        long actual = service.minMonSales(sales);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
+
+
 
 
